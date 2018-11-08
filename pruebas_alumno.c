bea018128e283a437c322d191276e0839a64d89d
Nickk Cuevas <nico.cuevastrias@hotmail.com>
2018-11-08 16:17:34 -0300

    Add new tests for join function, plus, refactor on join

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -26,25 +26,61 @@ void pruebas_split() {
     char* cadena1 = "abc";
     char* cadena2 = "abc,def";
     char* cadena3 = "def";
-    // char* cadena4 = ",abc,def";
-    char sep_vacio = '\0'; 
+    char* cadena4 = ",,abc,def";
+    char sep_vacio = '\0';
     char coma = ',';
+
     char** subset1 = split(cadena1, sep_vacio);
     char** subset2 = split(cadena1, coma);
     char** subset3 = split(cadena2, coma);
+    char** subset4 = split(cadena4, coma);
+
+    // size_t tam_subset1 = 2;
+    // size_t tam_subset2 = 2;
+    // size_t tam_subset3 = 3;
+    // size_t tam_subset4 = 5;
+
+
 
     /* Inicio de pruebas */
-    print_test("CADENA NO VACIA con separador vacio es NULL ", NULL == subset1);
+    print_test("CADENA NO VACIA con separador vacio es NULL ", strcmp(cadena1, subset1[0]) == 0);
     print_test("CADENA NO VACIA con separador no encontrado es Cadena ", ( (*cadena1 == *subset2[0]) && (NULL == subset2[1]) ) );
     print_test("CADENA NO VACIA con separador encontrado en subset1 es valido ", strcmp(cadena3, subset3[1]) == 0 );
-    print_test("CADENA NO VACIA con separador encontrado es sub Sets de cadena  ", ( (strcmp(cadena1, subset3[0]) == 0) && (strcmp(cadena3, subset3[1]) == 0) ));
+    print_test("CADENA NO VACIA con separador encontrado es sub Sets de cadena  ", ( (strcmp(cadena1, subset3[0]) == 0) && (strcmp(cadena3, subset3[1]) == 0) && (subset3[2] == NULL) ) );
+    print_test("CADENA NO VACIA con separador consecutivo es sub Sets de cadena  ", ( (strcmp(&sep_vacio, subset4[0]) == 0) && (strcmp(&sep_vacio, subset4[1]) == 0) ));
+    
+    free_strv(subset1);
     free_strv(subset2);
     free_strv(subset3);
+    free_strv(subset4);
 }
 
 
 void pruebas_join(){
+    printf("INICIO DE PRUEBAS CON FUNCION JOIN\n");
 
+    char coma = ',';
+    char* cadena0 = "";
+    char* cadena1 = "abc,def";
+    char* cadena2 = "abc";
+    char** subset0 = split(cadena0, coma);
+    char** subset1 = split(cadena1, coma);
+    char** subset2 = split(cadena2, coma);
+    char* j_cadena0 = join(subset0, coma);
+    char* j_cadena1 = join(subset1, coma);
+    char* j_cadena2 = join(subset2, coma);
+
+    print_test("ARREGLO VACIO devuelve una cadena vacia", ( strcmp(cadena0, j_cadena0) == 0 ) );
+    print_test("ARREGLO NO VACIO con cadenas no nulas devuelve una cadena concatenada a traves de separador ", ( strcmp(cadena1, j_cadena1) == 0 ) );
+    print_test("ARREGLO NO VACIO con cadenas no nulas con separador no encontrado devuelve una cadena concatenada ", ( strcmp(cadena2, j_cadena2) == 0 ) );
+
+    free_strv(subset0);
+    free_strv(subset1);
+    free_strv(subset2);
+
+    free(j_cadena0);    
+    free(j_cadena1);
+    free(j_cadena2);
 }
 
 
@@ -72,5 +108,6 @@ void pruebas_free() {
 
 void pruebas_alumno(){
     pruebas_split();
+    pruebas_join();
     pruebas_free();
 }
--- strutil.c
+++ strutil.c
@@ -6,6 +6,13 @@
 #include <stdio.h>
 
 
+void printer_elementos(char** arreglo){
+    for (int i=0; arreglo[i] != NULL; i++){
+            printf("ELEMENTO DE POSICION: %d :  %s \n", i, arreglo[i]);
+    };
+}
+
+
 int contar_apariciones(const char* cadena, char sep, size_t tam){
     int apariciones = 0;
     for (int i=0; i < tam; i++){
@@ -36,7 +43,6 @@ bool guardar_subset(const char* cadena, size_t tam_subset, int inicio, int fin, 
 
     
 char** split(const char * cadena, char sep){
-    if (sep == '\0') return NULL;
     size_t largo_cadena = strlen(cadena);
     size_t cant_subsets = contar_apariciones(cadena, sep, largo_cadena) + 1;
     char** subsets = malloc(sizeof(char*) * (cant_subsets + 1));
@@ -48,7 +54,7 @@ char** split(const char * cadena, char sep){
         if (cadena[i] == sep){
             bool guardo = guardar_subset(cadena, caracteres, pivot_subset_ini, i, pos_subset, subsets);
             if (! guardo){
-                free(subsets);
+                free_strv(subsets);
                 return NULL;
             }
             pivot_subset_ini = i + 1;
@@ -62,7 +68,7 @@ char** split(const char * cadena, char sep){
     if (caracteres > 0){
         bool guardo = guardar_subset(cadena, caracteres, pivot_subset_ini, pivot_subset_ini + caracteres, pos_subset, subsets);
         if (! guardo){
-            free(subsets); // TODO: Refactor this using free_strv instead.
+            free_strv(subsets);
             return NULL; 
         }
     }
@@ -73,34 +79,30 @@ char** split(const char * cadena, char sep){
 
 char* join(char** subset, char sep){
     if (! subset) return NULL;
-    char* pivot = *subset;
+    char** pivot_subset1 = subset;
+    size_t tam_subset = 1; // + NULL
     size_t tam_cadena = 0;
-    while (pivot){
-        tam_cadena = tam_cadena + strlen(pivot++);
+    while (*subset != NULL){
+        tam_cadena = tam_cadena + strlen( *(subset++) );
+        tam_subset++;
     }
-    char* cadena = malloc((sizeof(char) * (tam_cadena + 1))); // + '\0'
-    char* pivot2 = *subset;
-    while (pivot2){
-        mystrcat(cadena, pivot2++);
-        // hay que atrapar la posicion donde termina, "la anterior".
-        // concatenate(cadena, sep);
+    char* cadena = malloc( sizeof(char) * (tam_cadena + tam_subset + 1) ); // + '\0'
+    if (! cadena) return NULL;
+    char* puntero_cadena = cadena;
+    while (*pivot_subset1 != NULL){
+        puntero_cadena = mystrcat(puntero_cadena, *(pivot_subset1++));
+        if (*pivot_subset1){
+            strncpy(puntero_cadena, &sep, 1);
+        }
     }
+    *(puntero_cadena + 1) = '\0';
     return cadena;
 }
 
 
 void free_strv(char* arreglo[]){
-    //int tam = (int)strlen((char*)arreglo);
     for (int i=0; arreglo[i] != NULL; i++){
         free(arreglo[i]);
     }
     free(arreglo);
 }
-
-
-
-// join
-// un arreglo con cadenas. ["hola", "como estas"]
-// strdup(array[0])      && OJO: strcopy memoria estatica.(NO USAR)
-// strcat("hola", "como estas") -> "holacomo estas" MIRAR DOCUMENTACION MEM DINAMICA
-// "hola, como estas"
